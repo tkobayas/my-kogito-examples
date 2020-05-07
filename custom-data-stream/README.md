17adbdde600a7c7c5a50182dc3020d9f3a1fd33a
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-05-07 18:09:49 +0900

    static factory method

---
+++ custom-data-stream/README.md
@@ -0,0 +1,3 @@
+```
+curl -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{"eventData":[{"type":"temperature","value":35}, {"type":"temperature","value":20}]}' http://localhost:8080/warnings
+```
--- custom-data-stream/src/main/java/org/example/alerts/MyDataStream.java
+++ custom-data-stream/src/main/java/org/example/alerts/MyDataStream.java
@@ -4,6 +4,11 @@ import org.kie.kogito.rules.units.ListDataStream;
 
 public class MyDataStream<T> extends ListDataStream<T> {
 
+    //@Override
+    public static <T> MyDataStream<T> createStream() {
+        return new MyDataStream<>();
+    }
+
     @Override
     public void append(T value) {
         System.out.println("************** MyDataStream.append()");
