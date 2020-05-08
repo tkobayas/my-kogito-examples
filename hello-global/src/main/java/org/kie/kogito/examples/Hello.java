84d8866a69c9cd883773578fefcd16198ea192b7
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-05-08 14:09:33 +0900

    global example. not working

---
+++ hello-global/src/main/java/org/kie/kogito/examples/Hello.java
@@ -0,0 +1,45 @@
+/**
+ *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.kie.kogito.examples;
+
+import java.util.List;
+
+import org.kie.kogito.rules.DataSource;
+import org.kie.kogito.rules.DataStore;
+import org.kie.kogito.rules.RuleUnitData;
+
+public class Hello implements RuleUnitData {
+	DataStore<String> messages = DataSource.createStore();
+
+	List<String> resultList;
+
+	public DataStore<String> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(DataStore<String> messages) {
+		this.messages = messages;
+	}
+
+	public List<String> getResultList() {
+		return resultList;
+	}
+
+	public void setResultList(List<String> resultList) {
+		this.resultList = resultList;
+	}
+
+}
