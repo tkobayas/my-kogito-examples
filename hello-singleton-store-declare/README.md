afae698c5449d23689572e28d85e804ce247efa9
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-05-08 17:12:13 +0900

    SingletonStore with a custom Class

---
+++ hello-singleton-store-declare/README.md
@@ -0,0 +1,3 @@
+```
+curl -H "Content-Type: application/json" -X POST -d '{"result":{} ,"messages":["hello"]}' http://localhost:8080/hello
+```
---
+++ hello-singleton-store-declare/src/main/java/org/kie/kogito/examples/Result.java
@@ -0,0 +1,15 @@
+package org.kie.kogito.examples;
+
+public class Result {
+
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
