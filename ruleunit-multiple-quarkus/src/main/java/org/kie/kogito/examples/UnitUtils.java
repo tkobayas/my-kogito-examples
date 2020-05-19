82bb4b082d30bcbe1f9576527a9d945364d276a1
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-05-19 12:08:39 +0900

    multiple ruleunit example

---
+++ ruleunit-multiple-quarkus/src/main/java/org/kie/kogito/examples/UnitUtils.java
@@ -0,0 +1,29 @@
+package org.kie.kogito.examples;
+
+import org.drools.core.base.WrappedStatefulKnowledgeSessionForRHS;
+import org.drools.model.Drools;
+import org.drools.modelcompiler.consequence.DroolsImpl;
+import org.kie.api.runtime.KieRuntime;
+import org.kie.kogito.Application;
+import org.kie.kogito.rules.DataStore;
+import org.kie.kogito.rules.RuleUnit;
+import org.kie.kogito.rules.RuleUnits;
+
+public class UnitUtils {
+
+    public static void runAdultUnit(Drools drools) {
+        KieRuntime kieRuntime = drools.getRuntime(KieRuntime.class);
+        Application app = ((WrappedStatefulKnowledgeSessionForRHS)kieRuntime).getApplication();
+        RuleUnits ruleUnits = app.ruleUnits();
+        
+        DataStore<Person> persons = (DataStore<Person>)kieRuntime.getGlobal("persons");
+        Results results = (Results)kieRuntime.getGlobal("results");
+
+        RuleUnit<AdultUnit> adultUnit = ruleUnits.create(AdultUnit.class);
+        AdultUnit adultData18 = new AdultUnit(persons, 18);
+        adultData18.setResults(results);
+
+        adultUnit.createInstance(adultData18, "adult18");
+        ((DroolsImpl)drools).asKnowledgeHelper().run("adult18");
+    }
+}
